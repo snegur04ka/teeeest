725debbd4179486d03dd39e54ffe10a8d81ae2fb
dairchik <temirbekovdair66@gmail.com>
2023-06-17 12:48:00 +0300

    qwert

--- js/owerlay.js
+++ js/owerlay.js
@@ -3,12 +3,15 @@ const owerlayFunc = () => {
 const korzina = document.querySelector(".header__right--item")
 const drawer = document.querySelector(".drawer")
 const close = document.querySelector(".drawer__right--clos")
+const body = document.querySelector("body")
 korzina.addEventListener("click", () => {
     drawer.style.display = "block"
+    body.style.owerflow = "hidden"
 })
 
 close.addEventListener("click", () => {
     drawer.style.display = "none"
+    body.style.owerflow = "auto"
 })
 //
 }
